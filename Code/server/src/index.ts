a719c50e0896a837d164d6a373e1dc3628790a44
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-06-05 11:18:36 +0200

    Connect to database, also pass it to graphql
    Imported the database, and sent the database to the graphql through
    Apollo Server as context

--- Code/server/src/index.ts
+++ Code/server/src/index.ts
@@ -1,15 +1,29 @@
 import "dotenv/config"
 import express, { Application } from "express"
 import { ApolloServer } from "apollo-server-express"
+import { typeDefs, resolvers } from "./graphql"
+import { connectDatabase } from "./database"
 
 const app = express()
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const mount = async (app: Application) => {
+  const db = await connectDatabase()
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }),
+  })
+  //giving an await server.start() error so used below to fixed it.
+  server.start().then((res) => {
+    server.applyMiddleware({ app, path: "/api" })
+  })
 
-server.start().then((res) => {
-  server.applyMiddleware({ app, path: "/api" })
-})
+  app.listen(process.env.PORT)
 
-app.listen(process.env.PORT)
+  console.log(`[app]: http://localhost:${process.env.PORT}`)
 
-console.log(`Server started on port ${process.env.PORT}`)
+  const listings = await db.listings.find({}).toArray()
+  console.log(listings)
+}
+
+mount(express())
