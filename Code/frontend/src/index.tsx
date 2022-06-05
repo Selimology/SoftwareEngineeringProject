36685bbc7d11826f1adb382bc8c8867e95629a62
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-06-05 23:54:10 +0200

    fil directory fixed after name change

--- Code/frontend/src/index.tsx
+++ Code/frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Listings from "./Component/Listings"
+import Listings from "./Components/Listings"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(<Listings title="real estate management system" />)
