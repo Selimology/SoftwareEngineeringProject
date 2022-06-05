9f0c6235b811b17f4dcb6d414c3dc5c823e06f62
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-06-05 11:12:02 +0200

    typeDef for graphql

---
+++ Code/server/src/graphql/typeDefs.ts
@@ -0,0 +1,23 @@
+import { gql } from "apollo-server-express"
+
+export const typeDefs = gql`
+  type Listing {
+    id: ID!
+    title: String!
+    address: String!
+    image: String!
+    price: Int!
+    numOfGuests: Int!
+    numOfBaths: Int!
+    numOfBeds: Int!
+    rating: Int!
+  }
+
+  type Query {
+    listings: [Listing!]!
+  }
+
+  type Mutation {
+    deletelisting(id: ID!): Listing!
+  }
+`
