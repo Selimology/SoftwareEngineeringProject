7c3092e5ee440ef6823f914d72c59faedb8bd2f4
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-06-05 11:21:40 +0200

    resolver that uses database context from mongodb

---
+++ Code/server/src/graphql/resolvers/Listings/index.ts
@@ -0,0 +1,43 @@
+import { IResolvers } from "@graphql-tools/utils"
+import { Database, Listing } from "../../../lib/types"
+import { ObjectId } from "mongodb"
+
+export const listingResolvers: IResolvers = {
+  Query: {
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray()
+    },
+  },
+
+  Mutation: {
+    deletelisting: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      const deleteResult = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id),
+      })
+      if (!deleteResult.value) {
+        throw new Error("Failed to delete listing")
+      }
+      return deleteResult.value
+    },
+  },
+
+  Listing: {
+    id: (listing: Listing): string => listing._id.toString(),
+    title: (listing: Listing) => listing.title,
+    image: (listing: Listing) => listing.image,
+    address: (listing: Listing) => listing.address,
+    price: (listing: Listing) => listing.price,
+    numOfGuests: (listing: Listing) => listing.numOfGuests,
+    numOfBeds: (listing: Listing) => listing.numOfBeds,
+    numOfBaths: (listing: Listing) => listing.numOfBaths,
+    rating: (listing: Listing) => listing.rating,
+  },
+}
