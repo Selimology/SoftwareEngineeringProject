6584badbb19f473486d19af13d1a5300a856f7bc
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-06-07 03:32:07 +0200

    Setting up apollo client

--- Code/frontend/src/index.tsx
+++ Code/frontend/src/index.tsx
@@ -1,6 +1,22 @@
 import React from "react"
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client"
 import ReactDOM from "react-dom/client"
 import Listings from "./Components/Listings"
 
+const client = new ApolloClient({
+  uri: "/api",
+  cache: new InMemoryCache(),
+})
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
-root.render(<Listings title="real estate management system" />)
+root.render(
+  <ApolloProvider client={client}>
+    <Listings title="real estate management system" />
+  </ApolloProvider>
+)
