96f945af209696c13d7653756d2dc9ec7c305fae
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-06-11 03:44:56 +0200

    Add new listings

--- Code/server/temp/seed.ts
+++ Code/server/temp/seed.ts
@@ -15,12 +15,36 @@ const seed = async () => {
         title: "Single bedroom located in the heart of downtown Tirana",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Skanderbeg_square_tirana_2016.jpg/1200px-Skanderbeg_square_tirana_2016.jpg",
-        address: "DonBosko, California",
+        address: "Qender, Tirana",
         price: 25000,
         numOfGuests: 3,
         numOfBeds: 2,
         numOfBaths: 2,
         rating: 3,
+      },
+      {
+        _id: new ObjectId(),
+        title: "Two bedroom villa close to the center of Tirana",
+        image:
+          "https://q-xx.bstatic.com/xdata/images/hotel/840x460/89634025.jpg?k=41cfb0b56f6eebc7e4872d67d855f06bf0f901672413fac661cff80e81d25b34&o=",
+        address: "Toptani, Tirana",
+        price: 45000,
+        numOfGuests: 2,
+        numOfBeds: 2,
+        numOfBaths: 3,
+        rating: 5,
+      },
+      {
+        _id: new ObjectId(),
+        title: "Luxury Apartment closed to Blloku",
+        image:
+          "https://q-xx.bstatic.com/xdata/images/hotel/840x460/175389360.jpg?k=f8f39ac8de2229fb701dba12100cb864d753d5791a0b583b97d64665b05f0eb1&o=",
+        address: "Blloku, Tirana",
+        price: 45000,
+        numOfGuests: 2,
+        numOfBeds: 2,
+        numOfBaths: 3,
+        rating: 5,
       },
     ]
     for (const listing of listings) {
