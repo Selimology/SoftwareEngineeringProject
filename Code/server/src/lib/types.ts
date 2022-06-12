2b5a3a1f5b304e55dfe862806d9435d29432133d
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-06-12 04:50:13 +0200

    data structure of User

--- Code/server/src/lib/types.ts
+++ Code/server/src/lib/types.ts
@@ -5,7 +5,21 @@ export interface Listing {
 }
 
 export interface User {
-  _id: ObjectId
+  //it will be authenticated by a 3rdparty which will return an id token in string format
+  _id: string
+  //store logging session
+  token: string
+  name: string
+  picture: string
+  //email
+  contact: string
+  //walletID if undefined, it means they didn't link to a wallet yet
+  walletId?: string
+  income: number
+  //one to many relationship - one user will hold references to many bookings
+  bookings: ObjectId[]
+  //one to many relationship - one user will hold references to many listings
+  listings: ObjectId[]
 }
 export interface Booking {
   _id: ObjectId
